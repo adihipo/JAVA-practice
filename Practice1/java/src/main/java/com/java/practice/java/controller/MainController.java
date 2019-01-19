fba80f9634cc03f22ce6d60110bfccb44fd6eea1
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2019-01-19 16:42:38 +0100

    chore(practice1)

---
+++ Practice1/java/src/main/java/com/java/practice/java/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.java.practice.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+  @GetMapping("/")
+  public String getHome() {
+    return "index";
+  }
+
+}
