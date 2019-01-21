8c634b26c3b225c04e630cc38896425555b01e3f
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2019-01-21 11:22:15 +0100

    feat(animal): Hello world and init classes

---
+++ Practice2-basics/animals/src/Animals.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animals {
+
+  private List<Animal> animals;
+
+  public Animals() {
+    animals = new ArrayList<>();
+  }
+
+}
---
+++ Practice2-basics/animals/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+  public static void main(String[] args) {
+    print();
+  }
+
+  static void print() {
+    System.out.println("Hello World!");
+  }
+}
