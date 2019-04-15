0cdd30632b49eca1969accf4e1c6afa669940162
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2019-04-15 10:14:30 +0200

    feat(JsonToPDF): PDF creation with PDFBox

--- JsonToPDF/src/com/bla/Main.java
+++ JsonToPDF/src/com/bla/Main.java
@@ -5,6 +5,12 @@ public class Main {
   public static void main(String[] args) {
     JsonFromAPI json = new JsonFromAPI();
     System.out.println(json.jsonGetRequest("http://dummy.restapiexample.com/api/v1/employee/1"));
+    PDFwriter pdFwriter = new PDFwriter();
+    try {
+      pdFwriter.createPDF();
+    } catch (Exception e) {
+      System.out.println("An error occured during PDF creation...");
+    }
   }
 
 }
